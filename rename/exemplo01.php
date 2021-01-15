7daa19006b4b753e649872ad34c1c91fb95cdf66
Gabriel Luna <gabrielluna2000@gmail.com>
2021-01-15 18:00:24 -0300

    Novas funções de Arquivo 15/01

---
+++ rename/exemplo01.php
@@ -0,0 +1,24 @@
+<?php
+
+$dir1 = "folder_1";
+$dir2 = "folder_2";
+
+if (!is_dir($dir1)) mkdir($dir1);
+if (!is_dir($dir2)) mkdir($dir2);
+
+$filename = "README.txt";
+
+if(!file_exists($dir1 . DIRECTORY_SEPARATOR . $filename)){
+    $file= fopen($dir1 . DIRECTORY_SEPARATOR . $filename, "w+");
+    fwrite($file, date("d/m/Y H:i:s"));
+    fclose($file);
+}
+
+rename(
+    $dir2 . DIRECTORY_SEPARATOR . $filename, 
+    $dir1 . DIRECTORY_SEPARATOR . $filename
+);
+
+echo "Arquivo movido com sucesso";
+
+?>
