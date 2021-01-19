899380bb5bb8aec28faf8d01f3d1eda895fbbc76
Gabriel Luna <gabrielluna2000@gmail.com>
2021-01-19 17:24:06 -0300

    Nova atualização com o Composer 19/01

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3d8df151599f4fa820159820ef0cd76d8dfa22d5',
+    'name' => 'gabriel/composer',
+  ),
+  'versions' => 
+  array (
+    'gabriel/composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3d8df151599f4fa820159820ef0cd76d8dfa22d5',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+  ),
+);
