3d8df151599f4fa820159820ef0cd76d8dfa22d5
Gabriel Luna <gabrielluna2000@gmail.com>
2021-01-19 16:10:37 -0300

    Recurso com Imagens 19/01

--- error/exemplo01.php
+++ error/exemplo01.php
@@ -1,9 +1,17 @@
 <?php
+ 
+function error_handler($code, $message, $file, $line){
 
-function error_handler(){
-
-    
-
+    echo json_encode(array(
+        "code" => $code,
+        "message" => $message,
+        "file" => $file,
+        "line" => $line
+    ));
 }
+
+set_error_handler("error_handler");
+
+echo 100 / 0;
 
 ?>
