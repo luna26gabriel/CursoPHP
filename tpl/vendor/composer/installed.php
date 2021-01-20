a085decb12ab27c2c3e79acc8741782172b13035
Gabriel Luna <gabrielluna2000@gmail.com>
2021-01-20 15:49:04 -0300

    Template tpl 20/01

---
+++ tpl/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4c1810abac422bc2f8755f0e5b02c5cab4589869',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c1810abac422bc2f8755f0e5b02c5cab4589869',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'edee683bf242f40cc75bee46a78759f6c1589dca',
+    ),
+  ),
+);
