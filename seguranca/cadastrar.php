46d26f3cfc11876f6c2498c32089e2c137b2d6ca
Gabriel Luna <gabrielluna2000@gmail.com>
2021-01-21 16:39:20 -0300

    Nova Atualização de Segurança 21/10

---
+++ seguranca/cadastrar.php
@@ -0,0 +1,33 @@
+<?php
+
+$email = $_POST['inputemail'];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+    "secret"=>"6LfmcDYaAAAAAI3dgWLXm_RE8jx-OPLDLChoX2zZ",
+    "response"=>$_POST['g-recaptcha-response'],
+    "remoteip"=>$_SERVER['REMOTE_ADDR']
+)));
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$recaptcha = json_decode(curl_exec($ch), true);
+
+curl_close($ch);
+
+//var_dump($recaptcha);
+
+
+if($recaptcha["success"] === true){
+    echo "OK, " . $_POST['inputemail'];
+} else {
+    header("Location: exemplo04.php");
+}
+
+
+
+?>
---
+++ seguranca/exemplo04.php
@@ -0,0 +1,8 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<form action="cadastrar.php" method="post">
+
+    <div class="g-recaptcha" data-sitekey="6LfmcDYaAAAAAF9r-uz-QUkeUx2XQNuHq6Rfp-Nr"></div>
+    <input type="email" name="inputemail">
+    <button type="submit">Enviar</button>
+
+</form>
