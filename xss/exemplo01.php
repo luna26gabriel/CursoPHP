6a470b6c89efb0bf97533ccf18d08bd83606c27c
Gabriel Luna <gabrielluna2000@gmail.com>
2021-01-22 15:46:17 -0300

    Alguns comentarios a mais 22/01

--- xss/exemplo01.php
+++ xss/exemplo01.php
@@ -6,8 +6,10 @@
 <?php
 
 if(isset($_POST['busca'])){
-    // echo strip_tags($_POST['busca'], "<strong>");
-    echo htmlentities($_POST['busca']);
+    // echo strip_tags($_POST['busca'], "<strong>");   retira as tags, depois da VIRGULA é o filtro
+    echo htmlentities($_POST['busca']); //faz as tags vivarem strings
+    
+    
 }
 
 ?>
